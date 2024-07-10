7328016de8f8257f19ace3acea59ab1c5597082c
James Meech <43966357+JamesTimothyMeech@users.noreply.github.com>
2024-07-10 11:38:34 +0100

    Different C_p for upper and lower

--- src/main.c
+++ src/main.c
@@ -2,7 +2,8 @@
 #include <uxhw.h>
 #include <math.h>
 
-double calculate_v_from_C_p(double C_p, double v_infinity)
+double 
+calculate_v_from_C_p(double C_p, double v_infinity)
 {
 	return v_infinity*sqrt(1 - C_p);
 }
@@ -10,16 +11,21 @@ double calculate_v_from_C_p(double C_p, double v_infinity)
 int
 main(int argc, char *  argv[])
 {
-	double	v_infinity, C_p, v;
+	double	v_infinity, C_p_upper, v_upper, C_p_lower, v_lower;
 
 	v_infinity = UxHwDoubleGaussDist(95.2 * 0.3048, (95.2 * 0.3048) / 100 );
 	printf("v_infinity = %lf\n", v_infinity);
 
-	C_p = UxHwDoubleUniformDist(-0.300, 0.992);
-	printf("C_p = %lf\n", C_p);
+	C_p_lower = UxHwDoubleUniformDist(-0.300, 0.992);
+	printf("C_p_lower = %lf\n", C_p_lower);
 
-	v = calculate_v_from_C_p(C_p, v_infinity);
-	printf("v = %lf\n", v);
+	C_p_upper = UxHwDoubleUniformDist(-3.954, -0.300);
+	printf("C_p_upper = %lf\n", C_p_upper);
+
+	v_lower = calculate_v_from_C_p(C_p_lower, v_infinity);
+	printf("v_lower = %lf\n", v_lower);
+	v_upper = calculate_v_from_C_p(C_p_upper, v_infinity);
+	printf("v_upper = %lf\n", v_upper);
 
 #ifdef DEBUG
 	printf("debug message\n");

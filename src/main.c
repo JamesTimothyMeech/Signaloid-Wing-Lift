e6271a01e16b5ad74f0763448117c37cebfb79e8
James Meech <43966357+JamesTimothyMeech@users.noreply.github.com>
2024-07-10 17:02:00 +0100

    Change Cp variable names to averages to be more representative of what they are (averages)

--- src/main.c
+++ src/main.c
@@ -16,7 +16,7 @@ calculate_lift_from_C_p(double C_p_upper, double C_p_lower, double v_infinity, d
 int
 main(int argc, char *  argv[])
 {
-	double	v_infinity, C_p_upper, C_p_lower, rho, lift, A, upper_x_c, lower_x_c;
+	double	v_infinity, C_p_upper_average, C_p_lower_average, rho, lift, A, upper_x_c, lower_x_c;
 
 	char buffer[1024];
 	double C_p_upper_a[kMaxData];
@@ -39,11 +39,7 @@ main(int argc, char *  argv[])
 
 	for(int i = 0 ; i < 29 ; i++)
 	{
-		fscanf(file_pointer, "%lf, %lf, %lf, %lf\n", &upper_x_c, &C_p_upper, &lower_x_c, &C_p_lower);
-		C_p_upper_a[i] = C_p_upper;
-		C_p_lower_a[i] = C_p_lower;
-		upper_x_c_a[i] = upper_x_c;
-		lower_x_c_a[i] = lower_x_c;
+		fscanf(file_pointer, "%lf, %lf, %lf, %lf\n", &upper_x_c_a[i], &C_p_upper_a[i], &lower_x_c, &lower_x_c_a[i]);
 	}
 
 	rho = 1.225;
@@ -51,24 +47,24 @@ main(int argc, char *  argv[])
 	v_infinity = 95.2 * 0.3048;
 	printf("v_infinity = %lf\n", v_infinity);
 
-	C_p_upper = 0;
-	C_p_lower = 0;
+	C_p_upper_average = 0;
+	C_p_lower_average = 0;
 	
 
 	for(int i = 0 ; i < kMaxData - 1 ; i++)
 	{
-		C_p_upper = C_p_upper + 0.5 * (C_p_upper_a[i] + C_p_upper_a[i+1]) * (upper_x_c_a[i+1] - upper_x_c_a[i]);
+		C_p_upper_average = C_p_upper_average + 0.5 * (C_p_upper_a[i] + C_p_upper_a[i+1]) * (upper_x_c_a[i+1] - upper_x_c_a[i]);
 		// We need to do this because there are only 27 valid C_p readings for the coefficient of pressure below the airfoil
 		if (i < 26)
 		{
-			C_p_lower = C_p_lower + 0.5 * (C_p_lower_a[i] + C_p_lower_a[i+1]) * (lower_x_c_a[i+1] - lower_x_c_a[i]);
+			C_p_lower_average = C_p_lower_average + 0.5 * (C_p_lower_a[i] + C_p_lower_a[i+1]) * (lower_x_c_a[i+1] - lower_x_c_a[i]);
 		}
 	}
 
-	printf("C_p_upper = %lf\n", C_p_upper);
-	printf("C_p_upper = %lf\n", C_p_lower);
+	printf("C_p_upper_average = %lf\n", C_p_upper_average);
+	printf("C_p_upper_average = %lf\n", C_p_lower_average);
 
-	lift = calculate_lift_from_C_p(C_p_upper, C_p_lower, v_infinity, rho, A);
+	lift = calculate_lift_from_C_p(C_p_upper_average, C_p_lower_average, v_infinity, rho, A);
 	printf("lift = %lf\n", lift);
 
 #ifdef DEBUG
